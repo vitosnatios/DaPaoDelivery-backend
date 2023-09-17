237c87618df42865e4a99cea4164b6f1cea6ae96
vitosnatios <vitosdeveloper@gmail.com>
2023-09-17 02:48:50 -0300

    cors configured

--- src/app.js
+++ src/app.js
@@ -7,7 +7,13 @@ require('dotenv').config();
 const OrdersRoute_1 = __importDefault(require("./routes/OrdersRoute"));
 const express = require('express');
 const app = express();
+var cors = require('cors');
 const port = process.env.PORT || 3000;
+var corsOptions = {
+    origin: process.env.FRONTEND_BASE_URL,
+    optionsSuccessStatus: 200,
+};
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/orders', new OrdersRoute_1.default().getRouter());
 app.listen(3000, () => console.log('Server connected on port ' + port));
--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,16 @@ require('dotenv').config();
 import OrdersRoute from './routes/OrdersRoute';
 const express = require('express');
 const app = express();
+var cors = require('cors');
 
 const port = process.env.PORT || 3000;
 
+var corsOptions = {
+  origin: process.env.FRONTEND_BASE_URL,
+  optionsSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use('/api/orders', new OrdersRoute().getRouter());
 
