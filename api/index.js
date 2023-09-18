09d862c827221e6fa9ce7d753042a092bfcd3f7e
vitosnatios <vitosdeveloper@gmail.com>
2023-09-18 02:11:09 -0300

    import fix

--- api/index.js
+++ api/index.js
@@ -3,5 +3,5 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const app_1 = __importDefault(require("../src/app"));
-exports.default = app_1.default;
+const app_js_1 = __importDefault(require("../src/app.js"));
+exports.default = app_js_1.default;
