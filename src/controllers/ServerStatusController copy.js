b705a1f814c05a225ed34044cee3d8d15f9f5036
vitosnatios <vitosdeveloper@gmail.com>
2023-09-18 02:35:44 -0300

    ts-node setup

--- src/controllers/ServerStatusController copy.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ServerStatusController {
-    getServerStatus(req, res) {
-        res.sendStatus(200);
-    }
-}
-exports.default = ServerStatusController;
--- src/controllers/ServerStatusController.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-class ServerStatusController {
-    getServerStatus(req, res) {
-        res.status(200).json({ message: 'server ok' });
-    }
-}
-exports.default = ServerStatusController;
