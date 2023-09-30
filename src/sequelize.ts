85fe10527d542fffd656a81f258b6ea6d0b6eee9
vitosnatios <vitosdeveloper@gmail.com>
2023-09-29 23:50:53 -0300

    trying to fix sequelize problem

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,9 +1,11 @@
 import { Sequelize } from 'sequelize';
+import mysql2 from 'mysql2';
 
 const { DB_HOST, DB_USERNAME, DB_PASSWORD, DB_DATABASE } = process.env;
 
 const sequelize = new Sequelize({
   dialect: 'mysql',
+  dialectModule: mysql2,
   host: DB_HOST,
   username: DB_USERNAME,
   password: DB_PASSWORD,
